de6afcb7e75403a3163f5ccc184d7d8d8f51aa55
Usachan <yagamilaitoaspect@yahoo.co.jp>
2020-07-24 19:48:20 +0900

    Add last click date

--- js/common.js
+++ js/common.js
@@ -1,11 +1,14 @@
 const clickCountIndicater = document.getElementById("click-count");
 const cookieIcon = document.getElementById("cookie-icon");
-let clickCount = 0
+const lastClickTimeStamp = document.getElementById("last-click-time");
+let clickCount = 0;
+let lastClickTime = "";
 
 cookieIcon.addEventListener("click", () => {
     clickCount++
-    clickCountIndicater.innerHTML = clickCount
+    clickCountIndicater.innerHTML = clickCount;
+    let dateNow = new Date;
+    lastClickTime = `${dateNow.getFullYear()}年${dateNow.getMonth()}月${dateNow.getDate()}日${dateNow.getHours()}時${dateNow.getMinutes()}分${dateNow.getSeconds()}秒`
+    lastClickTimeStamp.innerHTML = lastClickTime;
 });
-
-
 
