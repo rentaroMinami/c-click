675b9554ae79cf07618eded75fffad9d3dde19ad
RentaroMinami <minami.rentaro@gmail.com>
2020-07-27 00:48:03 +0900

    3の倍数と３を含む数字でバカになる機能を追加

--- js/common.js
+++ js/common.js
@@ -1,5 +1,6 @@
 const CLICK_COUNT_INDICATER = document.getElementById("click-count");
 const COOKIE_ICON = document.getElementById("cookie-icon");
+const COOKIE_BACKGROUND = document.getElementById("cookie-bg")
 const LAST_CLICK_TIME_INDICATER = document.getElementById("last-click-time");
 let clickCount = 0;
 let lastClickTime = "";
@@ -11,12 +12,28 @@ function getFormattedDateStr(){
         ${dateNow.getHours()}時${dateNow.getMinutes()}分${dateNow.getSeconds()}秒\
         `;
     return lastClickTime;
-}
+};
+
+function isMultipleOfThree(inputNumber) {
+    return ((inputNumber %= 3) === 0)
+};
+function hasTheThree(inputNumber) {
+    const regex = /[0-9]*3[0-9]*/;
+    return regex.test(inputNumber);
+};
+function isNabeatsuNumber(inputNumber) {
+    return isMultipleOfThree(inputNumber) || hasTheThree(inputNumber);
+};
 
 COOKIE_ICON.addEventListener("click", () => {
     clickCount++;
     CLICK_COUNT_INDICATER.innerHTML = clickCount;
 
     LAST_CLICK_TIME_INDICATER.innerHTML = getFormattedDateStr();
+    console.log(isNabeatsuNumber(clickCount));
+    if(isNabeatsuNumber(clickCount)){
+        COOKIE_BACKGROUND.style.backgroundImage = "url(../image/nabeatsu.png)";
+    } else {
+        COOKIE_BACKGROUND.style.backgroundImage = "none";
+    };
 });
-
